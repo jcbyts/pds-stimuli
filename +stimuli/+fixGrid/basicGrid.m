2e9a3a6ded8f94f9c19cd8bdbd60d91987d5ad55
Jake <jacoby8s@gmail.com>
2017-03-30 11:10:38 -0400

    basic grid of points for eye tracker comparisons

--- +stimuli/+fixGrid/basicGrid.m
+++ +stimuli/+fixGrid/basicGrid.m
@@ -1,4 +1,4 @@
-function p=faceInvaders(p, state, sn)
+function p=basicGrid(p, state, sn)
 % FACEFORAGE module for PLDAPS open reception
 % Draws randomly scaled gaussians across the screen for reverse
 % correlation. Based loosely on ProceduralGarborium.m
@@ -6,56 +6,53 @@ function p=faceInvaders(p, state, sn)
 
 
 if nargin<3
-    sn='faceForage';
+    sn='fixGrid';
 end
 
 
 switch state
     case p.trial.pldaps.trialStates.framePrepareDrawing
-        p.trial.(sn).n.update
-        %     case p.trial.pldaps.trialStates.frameUpdate
         
         
     case p.trial.pldaps.trialStates.frameDraw
         
-        if p.trial.(sn).on
-            if p.trial.(sn).gazeContingent
-                p.trial.(sn).n.xy = [p.trial.eyeX p.trial.eyeY]+p.trial.(sn).xy;
-            end
-            p.trial.(sn).n.draw
-        end
+        Screen('DrawDots', p.trial.display.overlayptr, p.trial.(sn).xyPx, p.trial.(sn).szPx, p.trial.display.clut.black, [], 2);
+        Screen('DrawDots', p.trial.display.overlayptr, p.trial.(sn).xyPx, p.trial.(sn).szPx/2, p.trial.display.clut.white, [], 2);
         
     case p.trial.pldaps.trialStates.trialSetup
         
-        x0=(1:p.trial.(sn).gridSize) - p.trial.(sn).gridSize/2);
+        x0=(1:p.trial.(sn).gridSize) - p.trial.(sn).gridSize/2;
         
-        [xx,yy] = meshgrid(x0);
+        [xx,yy] = meshgrid(x0*p.trial.(sn).gridSpacing);
         
         p.trial.(sn).xyDeg = [xx(:) yy(:)];
-        p.trial.(sn).xyPx  = p.trial.(sn).xyDeg;
-        for i=1:size(p.trial.(sn).xyPx,1)
-            p.trial.(sn).xyPx(i,:) = pds.px2deg(p.trial.(sn).xyDeg(i,:), 
+        p.trial.(sn).xyPx  = p.trial.(sn).xyDeg';
+        for i=1:size(p.trial.(sn).xyPx,2)
+            p.trial.(sn).xyPx(:,i) = pds.deg2px(p.trial.(sn).xyDeg(i,:)',p.trial.display.viewdist, p.trial.display.w2px)+p.trial.display.ctr(1:2)';
+        end
         
+        p.trial.(sn).szPx = p.trial.display.ppd*p.trial.(sn).dotSize;
         
         
         
     case p.trial.pldaps.trialStates.experimentPostOpenScreen
         
-        if ~isfield(p.trial.(sn), 'fixRadius')
-            p.trial.(sn).fixRadius=1;
+        if ~isfield(p.trial.(sn), 'dotSize')
+            p.trial.(sn).dotSize=.7;
         end
         
         if ~isfield(p.trial.(sn), 'gridSize')
-            p.trial.(sn).gridSize=10;
+            p.trial.(sn).gridSize=11;
         end
         
         if ~isfield(p.trial.(sn), 'gridSpacing')
-            p.trial.(sn).gridSpacing = 5;
+            p.trial.(sn).gridSpacing = 2;
         end
        
         p.trial.(sn).on = true;
         
         
     case p.trial.pldaps.trialStates.trialCleanUpandSave
+        
         
 end
