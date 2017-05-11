c0f0499e6236e61591d2cbc0c0fa763e8bec21f4
Jake <jacoby8s@gmail.com>
2017-05-11 17:33:05 -0400

    Chelsea fixation parameters recorded

--- fromMarmieDemos/runChelseaFixation.m
+++ fromMarmieDemos/runChelseaFixation.m
@@ -4,8 +4,8 @@ clear settingsStruct
 Screen('Preference','ScreenToHead', 1,0,0);
 
 subject = 'Chelsea';
-rewardAmount  = .01;
-noiseContrast = .05;
+rewardAmount  = .025;
+noiseContrast = 0.075;
 
 % add modular trial functions
 settingsStruct.eyemarker.use=false;
@@ -13,17 +13,24 @@ settingsStruct.pldaps.useModularStateFunctions = true;
 settingsStruct.pldaps.trialMasterFunction='runModularTrial';
 settingsStruct.pldaps.save.mergedData=0;
 settingsStruct.session.subject=subject;
-settingsStruct.behavior.reward.defaultAmount=.05;
-
+settingsStruct.behavior.reward.defaultAmount=rewardAmount;
+settingsStruct.stimulus.rewardLevels = [.2 .4 .8 1 1.2 1.4];
 
 settingsStruct.eyelink.use=true;
 settingsStruct.eyelink.useRawData= true;
 
+if strcmp(subject, 'jnk')
+    settingsStruct.eyelink.use=false;
+    settingsStruct.eyelink.useRawData= false;
+    settingsStruct.mouse.useAsEyepos = true;
+end
+
+try
 cm = getpref('marmoview_calibration', subject);
 if ~isempty(cm)
     settingsStruct.eyelink.calibration_matrix = permute(cm, [2 1 3]);
 end
-   
+end
 
 %--------------------------------------------------------------------------
 % Add csd module
@@ -69,7 +76,11 @@ settingsStruct.(sn).stateFunction.requestedStates.trialPrepare=true;
 settingsStruct.(sn).stateFunction.requestedStates.framePrepareDrawing=true;
 settingsStruct.(sn).stateFunction.requestedStates.frameDraw=true;
 settingsStruct.(sn).stateFunction.requestedStates.trialCleanUpandSave=true;
-settingsStruct.(sn).contrast = 0.00;
+settingsStruct.(sn).contrast = noiseContrast;
+settingsStruct.(sn).gazeContingent = false;
+settingsStruct.(sn).xy = [1920 1080]/2;
+settingsStruct.(sn).pxsize = 20;
+settingsStruct.(sn).size = [10 10];
 
 settingsStruct.datapixx.use=true;
 settingsStruct.datapixx.LogOnsetTimestampLevel=0;
@@ -77,4 +88,8 @@ settingsStruct.display.useOverlay=2;
 settingsStruct.display.switchOverlayCLUTs=0;
 settingsStruct.pldaps.pause.preExperiment=true;
 
+%--- Chelsea specific fixation parameters
+settingsStruct.stimulus.fixWinRadius = 1.5;
+settingsStruct.stimulus.fixPointRadius = .3;
+settingStruct.stimulus.rewardLevels = [0.2 0.4 0.8 1.0 1.2 1.4 1.7 2.0];
 p=pldaps(@stimuli.fixflash.defaultParameters, subject, settingsStruct); p.run
