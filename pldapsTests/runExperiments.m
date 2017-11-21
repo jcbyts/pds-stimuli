bb54eb92e044747e2a61c9ceb38d53e25f8b69f2
Jake <jacoby8s@gmail.com>
2017-11-21 14:07:53 -0500

    update current Rig settings

--- pldapsTests/runExperiments.m
+++ pldapsTests/runExperiments.m
@@ -52,63 +52,9 @@ runFaceForageDotMapping(subject, pauseBeforeExperiment, ...
 
 %% Motion saccade task
 
-subject = 'test';
-settingsStruct = struct();
-% settingsStruct.display.destinationFactorNew = GL_ONE;
-
-pauseBeforeExperiment = 0;
-settingsStruct.display.colorclamp = 1;
-settingsStruct.display.normalizeColor = 1;
-
-settingsStruct.eyemarker.use=false;
-settingsStruct.pldaps.useModularStateFunctions = true;
-settingsStruct.pldaps.trialMasterFunction='runModularTrial';
-settingsStruct.pldaps.save.mergedData=0;
-settingsStruct.behavior.reward.defaultAmount= 0.05; %.015;
-
-settingsStruct.session.subject=subject;
-
-settingsStruct.stimulus.fixWinRadius = 1.5;
-settingsStruct.stimulus.fixPointRadius = .3;
-settingsStruct.stimulus.holdDuration = 15; % frames (counter, not continuous)
-settingsStruct.stimulus.RfCenterXy = [4 -4];
-settingsStruct.stimulus.DotCenterAngle(1) = 0;
-settingsStruct.stimulus.DotCenterAngle(2) = -90;
-
-% The reward function I coded up will switch which target has the higher
-% reward reate randomly with a switch rate of .1, meaning each trial a
-% random number is drawn and if it is less than .1, the reward scheme will
-% switch
-settingsStruct.stimulus.rewardUpdateFun = @stimuli.dotselection.rewardUpdateSwitchRule;
-settingsStruct.stimulus.rewardUpdateArgs = {.1}; % low rate, high rate
-
-settingsStruct.pldaps.draw.cursor.use = true;
-
-if pauseBeforeExperiment
-    settingsStruct.pldaps.pause.preExperiment = true;
-else
-    settingsStruct.pldaps.pause.preExperiment = false;
-end
-
-try
-    cm = getpref('marmoview_calibration', subject);
-    cm2 = cm(:,:,1)';
-    cm2(:,:,2) = cm(:,:,2)';
-    
-    settingsStruct.eyelink.calibration_matrix = cm2;
-    settingsStruct.eyelink.useRawData = true;
-catch me
-    throw(me)
-end
-
-if strcmp(subject, 'test')
-    settingsStruct.eyelink.use = false;
-    settingsStruct.mouse.useAsEyepos = true;
-end
-
-p = pldaps(@stimuli.dotselection.OneInRF, subject, settingsStruct);
-
-p.run
+% edit this function to see how it works (I will add parameter arguments
+% later)--Jake
+runDotSelection(subject, pauseBeforeExperiment)
 
 %% Spatial Mapping (Range, Fixation)
 runSpatialMapping(subject, pauseBeforeExperiment, ...
