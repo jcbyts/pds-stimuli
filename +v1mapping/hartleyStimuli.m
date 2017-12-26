d54b1fdf61b6f488849c6dbcb2977c85eb0ef1ee
Jake <jacoby8s@gmail.com>
2017-12-26 13:15:09 -0500

    last update to hartley in previous form before changing to new stimValue-based update

--- +v1mapping/hartleyStimuli.m
+++ +v1mapping/hartleyStimuli.m
@@ -29,138 +29,124 @@ switch state
         
     case p.trial.pldaps.trialStates.trialSetup
         
-        p.trial.display.sourceFactorNew = GL_ONE;
+        % BLEND FUNCTION MUST BE GL_ONE, GL_ONE FOR THIS MODULE TO WORK
+        p.trial.display.sourceFactorNew      = GL_ONE;
         p.trial.display.destinationFactorNew = GL_ONE;
+        
         Screen('BlendFunction', p.trial.display.ptr, p.trial.display.sourceFactorNew, p.trial.display.destinationFactorNew);
         
-        
-        
+
         % setup random seed
         p.trial.(sn).rngs.conditionerRNG=RandStream(p.trial.(sn).rngs.randomNumberGenerater, 'seed', p.trial.(sn).rngs.trialSeeds(p.trial.pldaps.iTrial));
         setupRNG=p.trial.(sn).rngs.conditionerRNG;
         
+        p.trial.(sn).hHart.contrast = p.trial.(sn).contrast;
+        p.trial.(sn).hHart.ppd = 1; % TODO: Why is M ppd and ppd 1
+        p.trial.(sn).hHart.M = p.trial.(sn).M;
         
-        
-%         if p.trial.(sn).on
-            p.trial.(sn).n=stimuli.hartley(p.trial.display.ptr, 'contrast', p.trial.(sn).contrast, ...
-                'ppd', 1, 'M', p.trial.(sn).M);
-            p.trial.(sn).n.setup;
+
+        p.trial.(sn).hHart.setup;
             
-            if isfield(p.trial.(sn), 'kx')
-                return
-            end
-%             p.trial.(sn).n.update;
-            p.trial.(sn).setupRNG=setupRNG;
+        if isfield(p.trial.(sn), 'kx')
+            return
+        end
+
             
-            p.trial.(sn).maxFrames=10e3;
-            p.trial.(sn).count=1;
-            p.trial.(sn).kx=nan(p.trial.(sn).maxFrames, p.trial.(sn).count);
-            p.trial.(sn).ky=nan(p.trial.(sn).maxFrames, p.trial.(sn).count);
-            p.trial.(sn).on=zeros(p.trial.(sn).maxFrames,p.trial.(sn).count);
-            p.trial.(sn).phi=nan(p.trial.(sn).maxFrames,p.trial.(sn).count);
-            p.trial.(sn).tf=nan(p.trial.(sn).maxFrames,p.trial.(sn).count);
+            p.trial.(sn).maxFrames = p.trial.pldaps.maxTrialLength * p.trial.display.frate;
+            p.trial.(sn).count = 1; % number shown
+            p.trial.(sn).kx  = nan(p.trial.(sn).maxFrames,  p.trial.(sn).count);
+            p.trial.(sn).ky  = nan(p.trial.(sn).maxFrames,  p.trial.(sn).count);
+            p.trial.(sn).on  = zeros(p.trial.(sn).maxFrames,p.trial.(sn).count);
+            p.trial.(sn).phi = nan(p.trial.(sn).maxFrames,  p.trial.(sn).count);
+            p.trial.(sn).tf  = nan(p.trial.(sn).maxFrames,  p.trial.(sn).count);
             
             % exponential decay
-            p.trial.(sn).M=p.trial.display.ppd; % grid size
-%             sort([-2.^(0:P.nOctaves)*P.Freq0 0 2.^(0:P.nOctaves)*P.Freq0]); % both positive and negative sied
-            p.trial.(sn).kxs=sort([-2.^(0:(p.trial.(sn).nOctaves-1))*p.trial.(sn).Freq0 0 2.^(0:(p.trial.(sn).nOctaves-1))*p.trial.(sn).Freq0]);
-            p.trial.(sn).kys=sort([-2.^(0:(p.trial.(sn).nOctaves-1))*p.trial.(sn).Freq0 0 2.^(0:(p.trial.(sn).nOctaves-1))*p.trial.(sn).Freq0]);
+            p.trial.(sn).M = p.trial.display.ppd; % grid size
+
+            freqs = sort([-2.^(0:(p.trial.(sn).nOctaves-1))*p.trial.(sn).Freq0 0 2.^(0:(p.trial.(sn).nOctaves-1))*p.trial.(sn).Freq0]);
+            p.trial.(sn).kxs = freqs;
+            p.trial.(sn).kys = freqs;
             
-        
-            on=ceil(exprnd(p.trial.(sn).OnDuration, p.trial.(sn).maxFrames,1));
-            off=round(exprnd(p.trial.(sn).OffDuration, p.trial.(sn).maxFrames,1));
-            on=min(on, p.trial.(sn).MaxDuration);
-            off=min(off, p.trial.(sn).MaxDuration);
-            step=on+off;
-            nUpdates=find(cumsum(step)>p.trial.(sn).maxFrames,1);
+            % generate exponentially distributed random variable using the
+            % exponential inverse cdf -- we call it this way so that we can
+            % pass in the random number generater argument
+            rnd = rand(setupRNG,  [p.trial.(sn).maxFrames,1]); % uniform random
+            on  = ceil( -p.trial.(sn).OnDuration .* log(rnd)); % convert to exponential
             
-            [kxg, kyg, ktfg]=ndgrid(p.trial.(sn).kxs, p.trial.(sn).kys, p.trial.(sn).tfs);
-            gridIx=randi(p.trial.(sn).setupRNG, numel(kxg), nUpdates, 1);
+            rnd = rand(setupRNG,  [p.trial.(sn).maxFrames,1]); % uniform random
+            off = ceil( -p.trial.(sn).OffDuration .* log(rnd)); % convert to exponential
+            
+            % truncate
+            on  = min(on,  p.trial.(sn).MaxDuration);
+            off = min(off, p.trial.(sn).MaxDuration);
+            
+            % precompute stimulus
+            step = on + off;
+            nUpdates = find(cumsum(step) > p.trial.(sn).maxFrames, 1);
+            
+            [kxg, kyg, ktfg] = ndgrid(p.trial.(sn).kxs, p.trial.(sn).kys, p.trial.(sn).tfs);
+            
+            gridIx = randi(setupRNG, numel(kxg), nUpdates, 1);
+            
             fr=0;
             for iStim=1:nUpdates
                 iFrames=fr+(1:on(iStim));
-                p.trial.(sn).kx(iFrames)=kxg(gridIx(iStim));
-                p.trial.(sn).ky(iFrames)=kyg(gridIx(iStim));
-                p.trial.(sn).on(iFrames)=1;
-                p.trial.(sn).tf(iFrames)=ktfg(gridIx(iStim));
-                p.trial.(sn).phi(iFrames)=rand*2*pi;
-                fr=iFrames(end)+off(iStim)-1;
+                p.trial.(sn).kx(iFrames)  = kxg(gridIx(iStim));
+                p.trial.(sn).ky(iFrames)  = kyg(gridIx(iStim));
+                p.trial.(sn).on(iFrames)  = 1;
+                p.trial.(sn).tf(iFrames)  = ktfg(gridIx(iStim));
+                p.trial.(sn).phi(iFrames) = rand(setupRNG)*2*pi;
+                fr = iFrames(end) + off(iStim) - 1;
             end
-            
-%             step=p.trial.(sn).OnDuration+p.trial.(sn).OffDuration;
-%             nUpdates=ceil(p.trial.(sn).maxFrames/step);
-%             [kxg, kyg, ktfg]=ndgrid(p.trial.(sn).kxs, p.trial.(sn).kys, p.trial.(sn).tfs);
-%             gridIx=randi(p.trial.(sn).setupRNG, numel(kxg), nUpdates, 1);
-%             
-%             for iStim=1:nUpdates
-%                 iFrames=(iStim-1)*step + (1:p.trial.(sn).OnDuration);
-%                 p.trial.(sn).kx(iFrames)=kxg(gridIx(iStim));
-%                 p.trial.(sn).ky(iFrames)=kyg(gridIx(iStim));
-%                 p.trial.(sn).on(iFrames)=1;
-%                 p.trial.(sn).tf(iFrames)=ktfg(gridIx(iStim));
-%                 p.trial.(sn).phi(iFrames)=nan;
-%             end
-%             p.trial.(sn).scale=nan(10e3, p.trial.(sn).n.count);
-%             p.trial.(sn).contrast=nan(10e3, p.trial.(sn).n.count);
-%         end
         
         
     case p.trial.pldaps.trialStates.experimentPostOpenScreen
         
-        if ~isfield(p.trial.(sn), 'OnDuration')
-            p.trial.(sn).OnDuration=2;
-        end
+        % --- set up default parameters
+        defaultArgs = {...
+            'OnDuration',           2, ... % time constant of on (frames)
+            'OffDuration',          2, ...
+            'MaxDuration',          20, ... % maximum time on (frames), truncates the exponential
+            'contrast',           	.2, ... % Michelson contrast of the gratings (DEPENDS ON BLEND FUNCTION)
+            'tfs',                  0, ...  % temporal frequencies showns
+            'nOctaves',             5, ...  % number of octaves to show above base frequency
+            'Freq0',                .1, ... % Base frequence (cycles/deg)
+            };
         
-        if ~isfield(p.trial.(sn), 'OffDuration')
-            p.trial.(sn).OffDuration=2;
-        end
-        
-        if ~isfield(p.trial.(sn), 'MaxDuration')
-            p.trial.(sn).MaxDuration=20;
-        end
-        
-        if ~isfield(p.trial.(sn), 'contrast')
-            p.trial.(sn).contrast=.2;
-        end
-        
-        
-        if ~isfield(p.trial.(sn), 'tfs')
-            p.trial.(sn).tfs=0; %[0 2 4 8 16];
-        end
-        
-        if ~isfield(p.trial.(sn), 'sfscale')
-            p.trial.(sn).sfscale=1;
-        end
-        
-        if ~isfield(p.trial.(sn), 'nOctaves')
-            p.trial.(sn).nOctaves=5; % grid size
-        end
-        
-        if ~isfield(p.trial.(sn), 'Freq0')
-            p.trial.(sn).Freq0=.1; % grid size
+        % step through argument pairs and add them to the module
+        for iArg = 1:2:numel(defaultArgs)
+            
+            field = defaultArgs{iArg};
+            val   = defaultArgs{iArg+1};
+            
+            if ~isfield(p.trial.(sn), field)
+                p.trial.(sn).(field) = val;
+            end 
         end
         
         
-        p.trial.(sn).M=p.trial.display.ppd; % grid size
-        p.trial.(sn).kxs=sort([0 2.^(0:p.trial.(sn).nOctaves)*p.trial.(sn).Freq0]);
-        p.trial.(sn).kys=sort([0 2.^(0:p.trial.(sn).nOctaves)*p.trial.(sn).Freq0]);
+        p.trial.(sn).M   = p.trial.display.ppd; % scale factor to convert degrees to pixels
+        freqs = sort([-2.^(0:(p.trial.(sn).nOctaves-1))*p.trial.(sn).Freq0 0 2.^(0:(p.trial.(sn).nOctaves-1))*p.trial.(sn).Freq0]);            
+        p.trial.(sn).kxs = freqs;
+        p.trial.(sn).kys = freqs;
         
         
-        p.trial.(sn).count=1;
+        p.trial.(sn).count = 1;
         [p.trial.(sn).kxgrid, p.trial.(sn).kygrid]=meshgrid(p.trial.(sn).kxs, p.trial.(sn).kys);
         
-        p.trial.(sn).rngs.randomNumberGenerater='mt19937ar';
-        p.trial.(sn).rngs.trialSeeds = randi(2^32, [3e3 1]);
+        p = stimuli.setupRandomSeed(p, sn);
+        
+        % --- instantiate Hartley object
+        p.trial.(sn).hHart = stimuli.hartley(p);
         
     case p.trial.pldaps.trialStates.trialCleanUpandSave
-%         if p.trial.(sn).on
-            ix=p.trial.iFrame:size(p.trial.(sn).kx,1);
-            p.trial.(sn).kx(ix,:)=[];
-            p.trial.(sn).ky(ix,:)=[];
-            p.trial.(sn).on(ix,:)=[];
-            p.trial.(sn).tf(ix,:)=[];
-            p.trial.(sn).phi(ix,:)=[];
-%             p.trial.(sn).scale(ix,:)=[];
-%         end
+        
+        ix = p.trial.iFrame+1:p.trial.(sn).maxFrames;
+        p.trial.(sn).kx(ix,:) = [];
+        p.trial.(sn).ky(ix,:) = [];
+        p.trial.(sn).on(ix,:) = [];
+        p.trial.(sn).tf(ix,:) = [];
+        p.trial.(sn).phi(ix,:)= [];
+
         
 end
