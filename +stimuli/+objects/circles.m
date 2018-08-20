2b2af99aea278afe02fe981281421481c5a70915
Jacob Yates <jacoby8s@gmail.com>
2018-08-20 15:37:35 -0400

    wip: dotmotion

---
+++ +stimuli/+objects/circles.m
@@ -0,0 +1,66 @@
+classdef circles < stimuli.objects.stimulus
+    % CIRCLES draws circles
+
+  properties
+    radius
+    position
+    weight
+    color
+  end
+
+
+  methods
+    function o = circles(varargin)
+      ip = inputParser();
+      ip.addOptional('radius', 300)
+      ip.addOptional('position', [960 540])
+      ip.addOptional('weight', 10)
+      ip.addOptional('color', [1 1 1])
+      ip.parse(varargin{:})
+
+      o.radius = ip.Results.radius;
+      o.position = ip.Results.position;
+
+    end
+
+    function setup(p)
+
+    end
+
+    function frameUpdate()
+
+    end
+
+    function frameDraw(o, p)
+
+      r = o.radius; % radius in pixels
+      
+      rect = kron([1,1],o.position) + kron(r(:),[-1, -1, +1, +1]);
+      if o.weight > 0,
+        Screen('FrameOval', p.trial.display.overlayptr, o.color, rect', o.weight);
+      else,
+        Screen('FillOval', p.trial.display.overlayptr, o.color, rect');
+      end
+      
+      if 0, % for debugging...
+        th = [0:0.05:1]*2*pi;
+%         r = ones(size(th))*r; % pixels
+        [r,th] = meshgrid(r,th);      
+
+        for ii = 1:size(r,2),
+          [x,y] = pol2cart(th(:,ii),r(:,ii));
+      
+          hold on;
+          plot(x+o.position(1),y+o.position(2),'-','Color',o.color);
+%           axis([-1, 1, -1, 1]*20);
+%           axis equal;
+          hold off
+        end
+      end % if 0|1
+      
+
+    end
+
+  end
+
+end
