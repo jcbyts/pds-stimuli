a62d528ba0bd812384bcb9c02dd2e77b1a5a589f
Jacob Yates <jacoby8s@gmail.com>
2018-11-30 16:26:30 -0500

    test conditions

--- pldapsTests/testConditions.m
+++ pldapsTests/testConditions.m
@@ -9,29 +9,33 @@ settingsStruct.session.experimentName = 'testConditions';
 c = {};
 N = nan;
 
-behavior = @forage_face_linear;
+behavior = @forage_face_randomwalk;
 % condition 1: colony pictures with face forage
 c{1} = combineConditions({@naturalimg_colonypics, behavior});
 c{1}.Nr = 1;
 c{1}.ConditionName = 'colonypics_faceforage';
-N(1) = 5;
+c{1}.pldaps.maxTrialLength = 10;
+N(1) = 1;
 
 % condition 2: hartleyFF autocorr with face forage
 c{2} = combineConditions({@hartleyFF_autocorr_2_contrast_010, behavior});
 c{2}.Nr = 2;
 c{2}.ConditionName = 'hartleyFF_autocorr_faceforage';
+c{2}.pldaps.maxTrialLength = 20;
 N(2) = 10;
 
 % condition 3: hartleyFF pBlank with face forage
 c{3} = combineConditions({@hartleyFF_pblank_050_contrast_010, behavior});
 c{3}.Nr = 3;
 c{3}.ConditionName = 'hartleyFF_pblank_faceforage';
+c{3}.pldaps.maxTrialLength = 20;
 N(3) = 10;
 
 % condition 4: hartleyFF frozen with face forage
 c{4} = combineConditions({@hartleyFF_frozen_contrast_010, behavior});
 c{4}.Nr = 4;
 c{4}.ConditionName = 'hartleyFF_frozen_faceforage';
+c{4}.pldaps.maxTrialLength = 20;
 N(4) = 5;
 
 conditions = buildConditions(c, N);
